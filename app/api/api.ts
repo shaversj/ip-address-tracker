d77fa1d47cdfd4a5b787b77f2b5ed5c0f4daabf1
Josh Shavers <shaversj@gmail.com>
2025-02-02 09:13:20 -0600

    Add new API

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,5 +3,5 @@ export async function fetchCurrentIPAddress() {
 }
 
 export async function fetchGeoLocation(ipAddress: string) {
-  return await fetch(`http://api.ipapi.com/api/${ipAddress}?access_key=${import.meta.env.VITE_API_KEY}`);
+  return await fetch(`https://api.ipapi.com/api/${ipAddress}?access_key=${import.meta.env.VITE_API_KEY}`);
 }
