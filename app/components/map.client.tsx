3bcfa749449d8ee55b5b697f77f13fd607c7911b
Josh Shavers <shaversj@gmail.com>
2025-02-02 13:08:13 -0600

    Update marker

--- app/components/map.client.tsx
+++ app/components/map.client.tsx
@@ -1,21 +1,11 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import { Icon, type LatLngExpression } from "leaflet";
+import { type LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import type { IPAddressInfo } from "~/types/types";
 import MapUpdater from "~/components/mapupdater";
-import iconLoc from "/icon-location.svg";
 
 export default function MapClient({ ipGeoLocation }: { ipGeoLocation: IPAddressInfo }) {
   let position: LatLngExpression = ipGeoLocation.latitude ? [ipGeoLocation?.latitude, ipGeoLocation?.longitude] : [51.505, -0.09];
-
-  // const LocationIcon = Icon.extend({
-  //   options: {
-  //     iconUrl: iconLoc,
-  //     iconSize: [46, 56],
-  //   },
-  // });
-  //
-  // const locationIconInstance = new LocationIcon();
 
   return (
     <section className={"relative z-20 h-[calc(100vh-300px)] md:h-[calc(100vh-280px)]"}>
