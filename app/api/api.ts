665b630a903ac4a915b6a533de4222d6292fb71f
Josh Shavers <shaversj@gmail.com>
2025-02-02 09:56:20 -0600

    Update api

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 export async function fetchCurrentIPAddress() {
-  return await fetch(`https://api.ipapi.com/api/check?access_key=${import.meta.env.API_ACCESS_KEY}`);
+  return await fetch(`https://api.ipapi.com/api/check?access_key=${import.meta.env.VITE_API_ACCESS_KEY}`);
 }
 
 export async function fetchGeoLocation(ipAddress: string) {
-  return await fetch(`https://api.ipapi.com/api/${ipAddress}?access_key=${import.meta.env.API_ACCESS_KEY}`);
+  return await fetch(`https://api.ipapi.com/api/${ipAddress}?access_key=${import.meta.env.VITE_API_ACCESS_KEY}`);
 }
